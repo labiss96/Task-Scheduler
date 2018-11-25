dc54f086a8cfaa251afaaa1635ecbf0045da9283
DESKTOP-KPAQHOV\Lee <labiss96@kookmin.ac.kr>
2018-11-25 20:20:58 +0900

    navigation drawer 구현(왼쪽 상단 메뉴바)했고, 과제 추가, 완료, 평균값 구하는거 넣어놨서 메인화면에서 과제 클릭하면 그 과제 string값을 detail activity title 텍스트에 보내서 구현했는데 아무것도 안뜨네..자살몰림

--- TaskScheduler/app/src/main/java/com/kmu/taskscheduler/DetailActivity.java
+++ TaskScheduler/app/src/main/java/com/kmu/taskscheduler/DetailActivity.java
@@ -1,13 +1,22 @@
 package com.kmu.taskscheduler;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity  extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle bundle)  {
         super.onCreate(bundle);
         setContentView(R.layout.activity_detail);
+
+        TextView title = (TextView) findViewById(R.id.title);
+        Intent intent = getIntent();
+        String title_contents = intent.getStringExtra("title");
+        title.setText(title_contents);
+
     }
 }
--- TaskScheduler/app/src/main/java/com/kmu/taskscheduler/MainActivity.java
+++ TaskScheduler/app/src/main/java/com/kmu/taskscheduler/MainActivity.java
@@ -1,19 +1,28 @@
 package com.kmu.taskscheduler;
 
+import android.support.design.widget.NavigationView;
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+        private DrawerLayout mDrawerLayout;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +31,40 @@ public class MainActivity extends AppCompatActivity {
 
             Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
             setSupportActionBar(myToolbar);
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+            mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+            NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    menuItem.setChecked(true);
+                    mDrawerLayout.closeDrawers();
+
+                    int id = menuItem.getItemId();
+                    switch (id) {
+                        case R.id.navigation_item_attachment:
+                            Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                            break;
+
+                        case R.id.navigation_item_images:
+                            Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                            break;
+
+                        case R.id.navigation_item_location:
+                            Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                            break;
+                    }
+
+                    return true;
+                }
+            });
 
             final ArrayList<String> tasks = new ArrayList<String>();
-            tasks.add("자료구조 Quiz");
+            tasks.add("Quiz");
             tasks.add("컴구 HW6");
             tasks.add("수학과문명 독후감");
 
@@ -35,10 +75,11 @@ public class MainActivity extends AppCompatActivity {
             listView.setOnItemClickListener(
                     new AdapterView.OnItemClickListener() {
                         @Override
-                        public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
-//                            String item = String.valueOf(parent.getItemAtPosition(i));
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                            String item = String.valueOf(parent.getItemAtPosition(position));
 //                            Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
                             Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                            intent.putExtra("TitleTask",tasks.get(position).toString());
                             startActivity(intent);
                         }
                     }
@@ -54,12 +95,32 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
-//                intent.putExtra();
-//            }
 
         }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_settings:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
